ef403b9a2342c2e3d4a6245af8746278f934e3dc
Thomas Seropian <thomas@seropian.io>
2020-12-29 19:21:58 +0100

    Homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div>
+    NBA Rankings per season
+    <ul id="example-1">
+      <li v-for="season in seasons" :key="season.year">
+        <a :href="'/season/' + season.year">
+          {{ season.year }}
+        </a>
+      </li>
+</ul>  
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    seasons: gql`query{
+      seasons {
+        year
+      }
+    }`,
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost'
 
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'http://localhost:4000/graphql'
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+Vue.use(VueApollo)
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  // inject apolloProvider here like vue-router or vuex
+  apolloProvider,
   render: h => h(App),
-}).$mount('#app')
+})
