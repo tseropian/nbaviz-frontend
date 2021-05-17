d54088a7d08f689424ec190412319c9a9725f6ec
Thomas Seropian <thomas@seropian.io>
2021-05-17 22:59:20 +0100

    Typos in About

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,10 +9,11 @@
     
     <p>Follow our updates on social media for the upcoming features.</p>
 
-    <p>Hoopcharts is run by Thomas, who grew up as a teenager in the 90s, watching NBA games on VHS. He is the New York Knicks, San Antonio Spurs and the Seattle SuperSonics.</p> 
+    <p>Hoopcharts is run by Thomas, who grew up as a teenager in the 90s, watching NBA games on VHS. He supports the New York Knicks, San Antonio Spurs and the Seattle SuperSonics (<em>:sad-emoji:</em>).</p> 
     
     <h2>Updates</h2>
     <ul>
+      <li>- 2021-05-17: 2021 regular season is here.</li>
       <li>- 2021-01-27: Imported regular seasons from 2010 to 2020</li>
       <li>- 2021-01-16: Fixed a bunch of bugs</li>
       <li>- 2021-01-06: Hoopcharts goes online with sample data from regular seasons 2018 to 2020</li>
@@ -20,7 +21,7 @@
     <h2>Credits</h2>
     <ul>
       <li>- <em>Basketball icon</em>: <a href="https://www.iconfinder.com/aomam.ss" target="_blank">AomAm</a></li>
-      <li>- <em>Hosting</em>: Vercel + Digital Ocean</li>
+      <li>- <em>Hosting</em>: Vercel + <a href="https://www.digitalocean.com/?refcode=573e01768cca" target="_blank">Digital Ocean</a></li>
     </ul>
     
   </div>
